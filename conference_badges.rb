413d9eda02d804e1ca684f82e472ae6acdc14209
blackwell <blackwellsmithiv@gmail.com>
2019-01-21 17:18:50 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
-# Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+def batch_badge_creator(attendees)
+  badge_list = []
+  attendees.each do |names|
+    badge_list << badge_maker(names)
+  end
+  return badge_list
+end
+
+def assign_rooms(attendees)
+  room_assignments = []
+  room = 1
+  attendees.each do |names|
+    room_assignments << "Hello, #{names}! You'll be assigned to room #{room}!"
+    room += 1
+  end
+  return room_assignments
+end 
+def printer(attendees)
+  assign_rooms(attendees)
+end
+    
+  
+  
+ 
